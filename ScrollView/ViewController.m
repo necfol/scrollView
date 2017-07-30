d5ee88db4a113eb5bd5a6d6f9cdd904cc81351c3
necfol <necfoll@gmail.com>
2017-07-30 13:38:00 +0800

    add scrollview content

--- ScrollView/ViewController.m
+++ ScrollView/ViewController.m
@@ -18,9 +18,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.scrollView.contentSize = CGSizeMake(0, 400);
+//    self.scrollView.contentSize = CGSizeMake(0, 400);
+    UIImageView *imgView = [[UIImageView alloc] init];
+    imgView.image = [UIImage imageNamed:@"minion"];
+    imgView.frame = CGRectMake(0, 0, imgView.image.size.width, imgView.image.size.height);
+    [self.scrollView addSubview:imgView];
+    self.scrollView.contentSize = CGSizeMake(imgView.frame.size.width, imgView.frame.size.height);
+}
+- (IBAction)left {
+    self.scrollView.contentOffset = CGPointMake(0, self.scrollView.contentOffset.y);
 }
 
+- (IBAction)top {
+    self.scrollView.contentOffset = CGPointMake(self.scrollView.contentOffset.x, 0);
+}
+- (IBAction)bottom {
+    self.scrollView.contentOffset = CGPointMake(self.scrollView.contentOffset.x, self.scrollView.contentSize.height - self.scrollView.frame.size.height);
+}
+- (IBAction)right {
+    self.scrollView.contentOffset = CGPointMake(self.scrollView.contentSize.width - self.scrollView.frame.size.width, 0);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
